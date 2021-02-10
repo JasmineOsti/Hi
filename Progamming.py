165774f11548179005ecb54df184795965bab22e
Lenovo Thinkbook <jasmineosti03@gmail.com>
2021-02-10 12:22:40 +0545

    Algebric

---
+++ Progamming.py
@@ -0,0 +1,29 @@
+# operation precedence
+# comments
+l=2
+b=4
+# area of rectangle
+'''hello everyone
+this is 
+multiline
+comment'''
+#a=l*b
+#print(a)
+#length=4
+#breadth=8
+#area=length*breadth
+#print('the area of rectangle is', area)
+#print('the area of rectangle is '+str(area))
+#print(f'the area of rectangle is {area}')
+#length=int(input('Enter the length:'))
+#breadth=int(input('Enter the breadth:'))
+#print('The area of rectangle is ',area)
+#radius=int(input('enter the radius of circle:'))
+#radius=float(input('Enter the radius:'))
+#radius=int(input('Enter the radius:'))
+#area=(22/7)*radius*radius
+#print('The area of circle is ',area)
+length=int(input('enter the length:'))
+breadth=int(input('enter the breadth:'))
+perimeter=2*(length+breadth)
+print('the perimeter of rectangle is',perimeter)
---
+++ main.py
@@ -0,0 +1,15 @@
+#a='soft'
+#b='warica'
+#print(a*2)
+#a='soft'
+#print(a[0:4])
+#print(a[-2:0:-1])
+#print(a[-2:0])
+#print(a[0:4:2])
+#print(a[:])
+#print(a[-1:0:-1])
+#print(a[::-1])
+#print(a[-1::-1])
+#print(a[-1:-1:-1])
+name='Johnson Paul'
+print(name[-1:-2])
